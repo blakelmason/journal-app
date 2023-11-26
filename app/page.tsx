f8c7bf62d0f7ce6ace630cd3da6820dbca655fcb
Blake Mason <blakelmason@gmail.com>
2023-11-25 19:11:43 -0800

    tiptap

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Page from "@/components/Page";
+import Tiptap from "@/components/TipTap";
+
+export default function Home() {
+  return (
+    <main>
+      <Page>
+        <Tiptap />
+      </Page>
+    </main>
+  );
+}
